dec1ca90fe0ac6f11c71f34522a3217c6289c303
AnamariaTi <tirdeaana@outlook.fr>
2023-04-11 12:43:03 +0100

    publicControlleur3

--- controller/publicController.php
+++ controller/publicController.php
@@ -28,3 +28,43 @@ if (isset($_GET['p'])) {
     $dataInstrumentHome = fetchInstrumentHome($dbConnect);
     include_once "../publicView/homepageView.php";
 }
+
+
+if(isset($_POST["firstname"],$_POST["lastname"],$_POST["message"])&&filter_var($_POST['mail']),FILTER_VALIDATE_EMAIL){
+    $firstname = htmlspecialchars(strip_tags(trim($_POST['firstname'])),ENT_QUOTES);
+    $lastname = htmlspecialchars(strip_tags(trim($_POST['lastname'])),ENT_QUOTES);
+    $mail =htmlspecialchars(strip_tags(trim($_POST['mail'])),ENT_QUOTES);
+    $message = htmlspecialchars(strip_tags(trim($_POST['message'])),ENT_QUOTES);
+   try{
+
+   //pour l'utilisateur
+   $mail =(new Email())
+   ->from(MAIL_FROM)
+   ->to($mail)
+   ->subject('Votre message a bien été posté !')
+   ->text('Votre message a bien été posté !\r\n '."\r\n". 'X-Mailer:PHP/'.phpversion())
+   ->html('<p>Votre message a bien  été posté !<br><br> </p>'); 
+   $mailer->send($mail);    
+
+    
+   //pour admin 
+   $mail =(new Email())
+   ->from(MAIL_FROM)
+   ->to(MAIL_ADMIN)
+   ->subject('Un nouveau message est arrivé sur votre site !')
+   ->text('Un nouveau message est arrivé sur votre site !\r\n '."\r\n". 'Poste par :'.$mail)
+   ->html('<p>Un nouveau message est arrivé sur votre site  !<br><br>'. 'Poste par : '.$mail.'</p>'); 
+   $mailer->send($mail);    
+
+     $e ="Merci pour votre commentaire" ; 
+ 
+
+    catch (Exception $e) {
+     $e = throw new Exception ("Un problème est survenu  !");
+
+    }
+
+}else if(!filter_var($_POST['mail']),FILTER_VALIDATE_EMAIL) {
+   $e = throw new Exception ("Veuillez rentrer un mail valide !") ; 
+}
+
