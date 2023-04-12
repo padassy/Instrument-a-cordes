51d6616a2d7d97082b4a2b8aff83242265b8ce55
padassy <pierre-alain.dassy@cf2000.onmicrosoft.com>
2023-04-12 16:07:44 +0200

    ajout sl

--- modele/modeleCategory.php
+++ modele/modeleCategory.php
@@ -2,24 +2,24 @@
 class Category{
 
     public int  $id ; 
-    public  string  $nomcategory ; 
+    public  string  $nameCategory ; 
   
 
 }
 
-function dataCategory($categorys,$dataCategory,$idcategory,$nomcategory):array {
+function dataCategory($categorys,$dataCategory,$idcategory,$nameCategory):array {
 $categorys =[]; 
 foreach($dataCategory as $item): 
     $category = new Category(); 
     $idcategory->$item['id']; 
-    $nomcategory->$item['nomcategory']; 
+    $nameCategory->$item['nameCategory']; 
     $categorys[]=$category; 
 endforeach; 
      return $categorys; 
 }
 # menu - to PDO with query
 function fetchCategory(PDO $db): array {
-    $sql ="SELECT id, nomcategory FROM category ORDER BY id ASC";
+    $sql ="SELECT id, nameCategory FROM category ORDER BY id ASC";
     try{
         $query=$db->query($sql);
     }catch(Exception $e){
@@ -34,7 +34,7 @@ function fetchCategory(PDO $db): array {
 // récupère une catégorie complète
 function recupCategoryById(PDO $db,int $idcategory):array|bool{
 
-    $recup = "SELECT c.id,c.nomcategory, ih.instrument_id,ih.category_id,i.id,i.title,i.description,i.history,i.technics,i.visible FROM category c INNER JOIN instrument_has_category ih ON c.id=ih.category_id INNER JOIN instrument i ON c.id = i.id where c.id=?;" ;
+    $recup = "SELECT c.id,c.nameCategory, ih.instrument_id,ih.category_id,i.id,i.title,i.description,i.history,i.technics,i.visible FROM category c INNER JOIN category_has_instrument ih ON c.id=ih.category_id INNER JOIN instrument i ON c.id = i.id where c.id=?;" ;
     $prepare = $db -> prepare($recup);
     try{
         $prepare->execute([$idcategory]);
