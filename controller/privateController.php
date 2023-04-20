0ff908a4788946dda643f0521ad6f26435a5dea0
Jenniferboo <bugara.jennifer@gmail.com>
2023-04-20 11:34:49 +0200

    pvControllerfintosbambinos

--- controller/privateController.php
+++ controller/privateController.php
@@ -1,36 +1,60 @@
 <h1>Private</h1>
 <?php
-if (isset($_GET['idcategory'])){
+if (isset($_GET['p'])) {
 
-    switch ($_GET['p']){
-      case "":
-        include "";
-        break;}
+    switch ($_GET['p']) {
+        case "contact":
+            
+            include_once "../publicView/contactView.php";
+            break;
+        case "article":
+            $assetInstruAll = fetchAllInstrument($dbConnect);
+            //var_dump($dataAllInstrument);
+            foreach($assetInstruAll as $item){
+               
+                $instruments[] = new modelInstrument($item);
+            }
+            //var_dump($instruments);
+            include "../publicView/articleView.php";
+            break;
+        case "admin":
+            include_once "../publicView/adminView.php";
+            break;
+        case "private":
+
+            $dataInstrumentHome = fetchInstrumentHome($dbConnect);
+
+            include_once "../publicView/privateView.php";
+            break;
+        
+            default:
+            include_once "../view/404.php";
     }
+       
+    
 
-  
+      
+    
+    }elseif (isset($_GET['idcategory'])&& ctype_digit($_GET['idCategory'])){
+        
+        $fetchCategory = fetchCategory($dbConnect);
+        
 
-if(isset($_GET['disconnect'])){
-    disconnect();
-    header("Location: ./");
-    exit();
-}
+        foreach($fetchCategory as $item){
 
+            $instruments[] = new modelInstrument($item);      
+         }
 
+         include_once "../publicView/articleView.php";
 
+    }else if(isset($_GET['disconnect'])){
+        disconnect();
+        header("Location: ./");
+        exit();
+    }else{
 
+    include "../privateView/privateView.php";
 
-
-
-
-
-/* code erreur*/
-/*if(is_bool($recupPost)){
-        # récupération du menu pour l'erreur 404
-        $recupMenu = getAllCategoryMenu($connectPDO);
-        // création de l'erreur pour la 404
-        $error = "Cet article n'existe plus";
-        // appel de la vue 404
-        include_once "../view/publicView/404View.php";*/
+ }
 
  
