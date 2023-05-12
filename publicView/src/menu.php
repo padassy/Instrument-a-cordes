998a3b8476901a211e0f7a97270290f0ab66e32e
Pierre-Alain <padassywebdev@gmail.com>
2023-05-12 16:38:30 +0200

    correction

--- publicView/src/menu.php
+++ publicView/src/menu.php
@@ -45,9 +45,9 @@
 <!-- Modal -->
 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content" style="background-color:#574D4D ; color: #FDF7F7;">
+        <div class="modal-content fontLato" style="background-color:#574D4D ; color: #FDF7F7; ">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Connexion à l'admin</h1>
+                <h1 class="modal-title fs-5 " id="staticBackdropLabel">Connexion à l'admin</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
